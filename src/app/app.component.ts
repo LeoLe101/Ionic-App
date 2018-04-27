0d380d1ee53b42a42a04d33b855a68b7203b0181
LeoLe101 <p.le6796@edmail.edcc.edu>
2018-04-27 13:41:35 -0700

    creating .scss file for login page and tweak the login.html. Implement simple login.ts that show loading process or fail during loging in. Still need more implementation as there isn't any backend yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,9 +47,9 @@ export class MyApp {
     });
   }
 
-  openPage(page: IPage): void {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+  // openPage(page: IPage): void {
+  //   // Reset the content nav to have just this page
+  //   // we wouldn't want the back button to show in this scenario
+  //   this.nav.setRoot(page.component);
+  // }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,25 +1,77 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  IonicPage, NavController, AlertController,
+  LoadingController, Loading, Alert
+} from 'ionic-angular';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 
 /**
  * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
  */
-
 @IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
+
 export class LoginPage {
+  // variables
+  loading: Loading;
+  register_user = { email: '', password: '' };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  // constructor for login page class
+  constructor(private nav_ctrl: NavController,
+    private auth: AuthServiceProvider,
+    private alert_ctrl: AlertController,
+    private loading_ctrl: LoadingController) { }
+
+  /*
+    navigate user to register page if they want to register
+    rather than login
+  */
+  public createAccount() {
+    this.nav_ctrl.push('RegisterPage');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  /**
+   * login to the application
+   */
+  public login() {
+    this.showLoading(); // call showLoading function below
+    this.auth.login(this.register_user).subcribe(
+      allowed => {
+        if (allowed)
+          this.nav_ctrl.setRoot('HomePage');
+        else
+          this.showError("We cannot find your tank");
+      },
+      error => {
+        this.showError(error);
+      }
+    );
   }
+
+  // show loading while the app is processing the user information
+  showLoading() {
+    this.loading = this.loading_ctrl.create({
+      content: 'Pumping your Scuba tank...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+  }
+
+  showError(message) {
+    this.loading.dismiss();
+    let alert = this.alert_ctrl.create({
+      title: 'Fail',
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+
+
+
 
 }
